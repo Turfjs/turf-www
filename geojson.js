788b9be6333f906cacae0a8cbc267c240736b159
Tom MacWright <tom@macwright.org>
2015-01-06 18:41:58 -0500

    Add GeoJSON defs

---
+++ geojson.js
@@ -0,0 +1,44 @@
+/**
+ * @typedef {Object} Polygon
+ * @description [GeoJSON Polygon geometry](http://geojson.org/geojson-spec.html#polygon):
+ * typically contained within a GeoJSON Feature
+ */
+
+/**
+ * @typedef {Object} Point
+ * @description [GeoJSON Point geometry](http://geojson.org/geojson-spec.html#point):
+ * typically contained within a GeoJSON Feature
+ */
+
+/**
+ * @typedef {Object} GeometryCollection
+ * @description [GeoJSON GeometryCollection](http://geojson.org/geojson-spec.html#geometrycollection)
+ */
+
+/**
+ * @typedef {Object} MultiLineString
+ * @description [GeoJSON MultiLineString geometry](http://geojson.org/geojson-spec.html#multilinestring):
+ * typically contained within a GeoJSON Feature
+ */
+
+/**
+ * @typedef {Object} MultiPoint
+ * @description [GeoJSON MultiPoint geometry](http://geojson.org/geojson-spec.html#multipoint):
+ * typically contained within a GeoJSON Feature
+ */
+
+/**
+ * @typedef {Object} LineString
+ * @description [GeoJSON LineString geometry](http://geojson.org/geojson-spec.html#linestring):
+ * typically contained within a GeoJSON Feature
+ */
+
+/**
+ * @typedef {Object} Feature
+ * @description [GeoJSON Feature](http://geojson.org/geojson-spec.html#feature-objects)
+ */
+
+/**
+ * @typedef {Object} FeatureCollection
+ * @description [GeoJSON FeatureCollection](http://geojson.org/geojson-spec.html#feature-collection-objects)
+ */
