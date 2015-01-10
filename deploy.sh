6add30a979a9e73a52ab1ac4fc44ec87af7ee545
morganherlocker <morgan.herlocker@gmail.com>
2015-01-10 02:03:56 -0500

    added CNAME generation

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 (cd _site; git init)
 (cd _site; git remote add origin git@github.com:turfjs/turf-www.git)
 (cd _site; git checkout -b gh-pages)
+(cd _site; echo turf.js > CNAME)
 (cd _site; git add .)
 (cd _site; git commit -m "Deploy")
 (cd _site; git push origin gh-pages -f)
