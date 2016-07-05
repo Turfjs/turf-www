08e9a331fc85c84752a9e6dd222b51e3549315f8
Tim Channell <tim.channell@gmail.com>
2016-07-05 13:01:56 -0400

    clean up old examples

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-(cd _site; git init)
-(cd _site; git remote add origin git@github.com:turfjs/turf-www.git)
-(cd _site; git checkout -b gh-pages)
-(cd _site; echo turfjs.org > CNAME)
-(cd _site; git add .)
-(cd _site; git commit -m "Deploy")
-(cd _site; git push origin gh-pages -f)
