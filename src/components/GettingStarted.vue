b173e499fa7b6b5aa0e71515c3fcfcc31dcd181d
Denis <carriere.denis@gmail.com>
2017-10-20 11:09:49 -0600

    Add Javascript highlighting for Getting Started

--- src/components/GettingStarted.vue
+++ src/components/GettingStarted.vue
@@ -5,28 +5,28 @@
       <p>There are a few different ways to get started using Turf, the documentation below will provide a couple of the common scenarios.</p>
       <h4>Using directly in the browser</h4>
       <p>Load the minified file via a script tag, this will expose a global variable named turf.</p>
-      <pre><code class="html">&lt;script src='https://npmcdn.com/@turf/turf/turf.min.js'&gt;&lt;/script&gt;   
+      <prism language="javascript">&lt;script src='https://npmcdn.com/@turf/turf/turf.min.js'&gt;&lt;/script&gt;
 &lt;script&gt;
     var bbox = turf.bbox(features);
-&lt;/script&gt;</code></pre>
+&lt;/script&gt;</prism>
       <p><strong>Note:</strong> The full build of Turf weighs around 500kb which is a fair bit of javascript to load so you probably only want to pull in the bits you need, see the instructions below on how to create a custom build.</p>
       <h4>Using in Node or with a build tool</h4>
       <p>If you're working in Node or with build tool (such as webpack, browserify or rollup) you can include individual turf modules in your project.</p>
-      <pre><code class="js">// Import your module of interest
+      <prism language="javascript">// Import your module of interest
 const turfCollect = require('@turf/collect');
 // And then use it
-turfCollect(points, polys, 'population', 'populationValues')</code></pre>
+turfCollect(points, polys, 'population', 'populationValues')</prism>
 
       <h4>Creating custom builds for use in the browser</h4>
       <p><strong>Step 1</strong> Create a folder and install whatever modules you are interested in via npm</p>
-        <pre><code class="js">npm install @turf/collect @turf/buffer</code></pre>
+        <pre><code class="js">$ npm install @turf/collect @turf/buffer</code></pre>
         <p><strong>Step 2</strong> Create a file called main.js in the root directory, in this include your required modules within a modules.exports statement</p>
-        <pre><code class="js">module.exports = {
-collect: require('@turf/collect'),
-buffer: require('@turf/buffer')
-};</code></pre>
+        <prism language="javascript">module.exports = {
+    collect: require('@turf/collect'),
+    buffer: require('@turf/buffer')
+};</prism>
       <p><strong>Step 3</strong> Run the following browserify command</p>
-      <pre><code class="js">browserify main.js -s turf > outTurf.js </code></pre>
+      <pre><code class="js">$ browserify main.js -s turf > outTurf.js </code></pre>
       <p><strong>Done</strong> You can now use your outTurf.js file where you want just like you would normally use Turf, eg load it via a script tag and call turf using the turf global variable.</p>
     </Col>
     <Col span="6" offset="1" class="highlight">
@@ -45,12 +45,14 @@ buffer: require('@turf/buffer')
 
 <script>
 import {Row, Col} from 'iview/src/components/grid'
+import Prism from 'vue-prism-component'
 
 export default {
   name: 'GettingStarted',
   components: {
     Row,
-    Col
+    Col,
+    Prism
   }
 }
 </script>
