cfd94358c2d87e582550194a13e2d0fd91461edf
James Beard <james@smallsaucepan.com>
2024-06-06 18:37:34 +1000

    Adding recommended trailingSlash setting. Noticing a lot of redirects during search indexing.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Turfjs", // Usually your GitHub org/user name.
   projectName: "turf-www", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
